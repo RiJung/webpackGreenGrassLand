5bb8808b98f5714fdd837f986fa023b3d73cd56c
Richard Junggebauer <jertiri@gmail.com>
2019-02-25 14:45:18 +0100

    3

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
-import './my-styles.less';
-const a = document.querySelector('body');
-if(a){
-    a.classList.add('with-styles');
+import './theme.less';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Home extends React.Component {
+    public render() {
+        return (
+            <>
+                <h1>Home</h1>
+                Works
+            </>
+        );
+    }
 }
-  
+
+const indexHtmlDOMFound = document.getElementById('app');
+if(indexHtmlDOMFound){
+    ReactDOM.render(<Home />, document.getElementById('app'));
+}
