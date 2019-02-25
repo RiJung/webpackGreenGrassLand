6c2878738272871830a4d430455496d57cca5ead
Richard Junggebauer <jertiri@gmail.com>
2019-02-25 14:25:50 +0100

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,61 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const config = {
-    // First, let's define an entry point for webpack to start its crawling.
-    entry: './src/index.tsx',
-    // Second, we define where the files webpack produce, are placed
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-        compress: true,
-        historyApiFallback: true,
-        host: "127.0.0.1",
-        port: 8081,
-        watchOptions: {
-            ignored: ['dist']
-        }
-    },
-    module: {
-        rules: [
-            { test: /\.(ts|tsx)$/, loader: "ts-loader", options: { configFile: "tsconfig.gui.json"} },
-            { 
-                test: /\.(le|c)ss$/, // .less and .css
-                use: [ 
-                    isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 
-                    'css-loader', 
-                    'less-loader'
-                ],
-            },
-        ]
-    },
-    // Add an instance of the MiniCssExtractPlugin to the plugins list
-    // But remember - only for production!
-    plugins: isProduction ? [new MiniCssExtractPlugin()] : [],
-    devtool: 'source-map',
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+const styleLoader = isDev ? 'style-loader' : MiniCssExtractPlugin.loader;
+const cssLoader = {
+    loader: "css-loader",
+    options: {
+        camelCase: true,
+        localIdentName: '[local]--[hash:base64:5]',
+        minimize: isProd,
+        sourceMap: isDev
+    }
+};
+const lessLoader = {
+    loader: "less-loader",
+    options: {
+        javascriptEnabled: true,
+        sourceMap: isDev
+    }
 };
 
-module.exports = config;
+module.exports = (env, argv) => {
+    return {
+        // First, let's define an entry point for webpack to start its crawling.
+        entry: './src/index.tsx',
+        // Second, we define where the files webpack produce, are placed
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+        },
+        devServer: {
+            contentBase: path.resolve(__dirname, 'public'),
+            compress: true,
+            historyApiFallback: true,
+            host: "127.0.0.1",
+            port: 8081,
+            watchOptions: {
+                ignored: ['dist']
+            }
+        },
+        module: {
+            rules: [
+                { test: /\.(ts|tsx)$/, loader: "ts-loader", options: { configFile: "tsconfig.gui.json" } },
+                {
+                    test: /\.(le|c)ss$/, // .less and .css
+                    use: [
+                        isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+                        'css-loader',
+                        'less-loader'
+                    ],
+                },
+            ]
+        },
+        // Add an instance of the MiniCssExtractPlugin to the plugins list
+        // But remember - only for production!
+        plugins: isProd ? [new MiniCssExtractPlugin()] : [],
+        devtool: 'source-map'
+    }
+};
